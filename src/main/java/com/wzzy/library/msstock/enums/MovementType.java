99b9c7c3d944dc72769539d09bfc3b80d6d5eac1
codewesleylima <gusterlima@hotmail.com>
2026-05-24 23:47:04 -0300

    feat(ms-stock): add MovementType enum

---
+++ src/main/java/com/wzzy/library/msstock/enums/MovementType.java
@@ -0,0 +1,12 @@
+package com.wzzy.library.msstock.enums;
+
+public enum MovementType {
+    ENTRY,
+    EXIT,
+    RESERVATION,
+    RETURN,
+    ADJUSTMENT,
+    DAMAGE,
+    LOSS,
+    TRANSFER
+}
