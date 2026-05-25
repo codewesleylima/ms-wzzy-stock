d1171e73bd1d83eac2c2568e1005da3e5e7528f0
codewesleylima <gusterlima@hotmail.com>
2026-05-24 23:46:32 -0300

    feat(ms-stock): initialize project structure
    - Add build configuration
    - Add Spring Boot application class
    - Add initial test skeleton

---
+++ src/test/java/com/wzzy/library/msstock/MsStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzzy.library.msstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
